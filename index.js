1b2f317ecf39d3d7c11e4d126c8efe72e23cb88a
fateme nouri <fatemenouri99@gmail.com>
2022-04-12 04:55:36 +0430

    working on restart

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const handOptions = {
     "scissors": "/assets/icon-scissors.svg"
 }
 
+let score = 0
+
 function pickHand(hand) {
-    const hands = document.getElementById('hands');
-    hands.style.display = "none";
+    const handsSection = document.getElementById('hands');
+    handsSection.style.display = "none";
 
     const pickedSection = document.getElementById('picked');
     pickedSection.style.display = "flex";
@@ -19,6 +21,63 @@ function pickHand(hand) {
     computerHand(hand);
 }
 
-function computerHand() {
+function computerHand(hand) {
+    let hands = ["rock", "paper", "scissors"];
+    let computerHand = hands[Math.floor(Math.random() * hands.length)];
+    document.getElementById("computerPickedImage").src = handOptions[computerHand];
+    const computerpicked = document.getElementById("computerpickedCircle")
+    computerpicked.classList.add(computerHand)
+    result(hand, computerHand)
+}
 
+function result(userHand, computerHand) {
+    if (userHand == "paper" && computerHand == "scissors") {
+        setResult("YOU LOSE!");
+    }
+    if (userHand == "rock" && computerHand == "paper") {
+        setResult("YOU LOSE!");
+    }
+    if (userHand == "scissors" && computerHand == "rock") {
+        setResult("YOU LOSE!");
+    }
+    if (userHand == "paper" && computerHand == "paper") {
+        setResult("It's a tie!");
+    }
+    if (userHand == "rock" && computerHand == "rock") {
+        setResult("It's a tie!");
+    }
+    if (userHand == "scissors" && computerHand == "scissors") {
+        setResult("It's a tie!");
+    }
+    if (userHand == "scissors" && computerHand == "paper") {
+        setResult("YOU WIN!");
+        setScore(score + 1);
+    }
+    if (userHand == "paper" && computerHand == "rock") {
+        setResult("YOU WIN!");
+        setScore(score + 1);
+    }
+    if (userHand == "rock" && computerHand == "scissors") {
+        setResult("YOU WIN!");
+        setScore(score + 1);
+    }
+};
+
+
+
+function setResult(result) {
+    document.querySelector(".result h1").innerText = result;
+}
+
+function setScore(newScore) {
+    score = newScore;
+    document.querySelector(".score h1").innerText = newScore;
+}
+
+function restart() {
+    let picked = document.getElementById("picked");
+    picked.style.display = "none";
+
+    let hands = document.getElementById("hands");
+    hands.style.display = "flex";
 }
